f5f2a4f20aa11c8320887821b69c02ba4475fe98
Arif Sulistiono <ap_sulistiono@yahoo.com>
2022-08-03 19:04:42 +0100

    Updated on 3 August 2022.

--- scrape.js
+++
@@ -1,5 +0,0 @@
-var page = require('webpage').create();
-                     page.open('http://fantasy.premierleague.com/a/team/13324/event/1', function () {
-                     console.log(page.content); //page source
-                     phantom.exit();
-                     });
